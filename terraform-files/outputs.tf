5affcb89841b5fb430b898eaf51da3d6f8e78bd5
anna <anna.aakerman@outlook.com>
2026-06-27 18:58:21 +0200

    removed unnecessary output, will update more later...

--- terraform-files/outputs.tf
+++ terraform-files/outputs.tf
@@ -1,28 +1,4 @@
-output "public_subnet_cidr" {
-  description = "Public IPs of the web server instances"
-  value       = var.public_subnet_cidr
-}
-
-output "private_subnet_cidr" {
-  description = "Private IPs of the web server instances"
-  value       = var.private_subnet_cidr
-}
-
-output "vpc_cidr" {
-  description = "VPC cidr"
-  value       = var.vpc_cidr
-}
-
 output "vpc_name" {
   description = "VPC name"
   value       = var.vpc_name
-}
-output "web_server_public_ips" {
-  description = "Public IPs of the web server instances"
-  value       = aws_instance.web_server[*].public_ip
-}
-
-output "web_server_private_ips" {
-  description = "Private IPs of the web server instances"
-  value       = aws_instance.web_server[*].private_ip
 }
