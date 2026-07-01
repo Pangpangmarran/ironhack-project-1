396fbf8d18dcda2d709f360e392e8f85dd462bab
anna <anna.aakerman@outlook.com>
2026-07-01 20:51:05 +0200

    Improvements to the files, ansible connection now works to all the resources created but there is an issue with getting the vote to properly connect with the backend. Still some details to fix.

--- terraform-files/outputs.tf
+++ terraform-files/outputs.tf
@@ -2,3 +2,11 @@ output "vpc_name" {
   description = "VPC name"
   value       = var.vpc_name
 }
+output "instance_information" {
+  description = "names and IPs of the created resources"
+  value       = {
+    name      = instance.tags["name"]
+    private_ip= instance.private_ip
+    Role      = instance.Role
+  }
+}
