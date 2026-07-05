32c67ec19fda0902a7d0df436339a29f407c6813
anna <anna.aakerman@outlook.com>
2026-07-05 19:18:08 +0200

    sg ids in output file and space, just space

--- terraform-files/outputs.tf
+++ terraform-files/outputs.tf
@@ -5,10 +5,18 @@ output "vpc_name" {
 output "instance_information" {
   description = "names and IPs of the created resources"
   value = {
-    for instance_key, instance_resource in aws_instance.ec2 : instance_key => {
+    for instance_key, instance_resource in aws_instance.ec2 : 
+    instance_key => {
       name       = instance_resource.tags["Name"]
       private_ip = instance_resource.private_ip
       Role       = instance_resource.tags["Role"]
     }
   }
+}
+output "security_groups_ids" {
+  description = "The IDs for the sg"
+  value = [
+    aws_security_group.backend_sg.id,
+    aws_security_group.frontend_sg.id
+  ]
 }
--- terraform-files/subnets.tf
+++ terraform-files/subnets.tf
@@ -26,7 +26,8 @@ variable "private_subnet_cidr" {
 variable "private_db_subnet_cidr" {
   type    = string
   default = "10.0.4.0/24"
-}
+} 
+
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidr
