b9a507e2c07b9b3f50762d172b0f562888a1b2fc
anna <anna.aakerman@outlook.com>
2026-07-14 19:41:17 +0200

    Final edits, working app!

--- terraform-files/security.tf
+++ terraform-files/security.tf
@@ -21,25 +21,25 @@ resource "aws_security_group" "frontend_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.vpc_cidr]
   }
   ingress {
     from_port   = 81
     to_port     = 81
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.vpc_cidr]
   }
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.vpc_cidr]
   }
   ingress {
     from_port   = 8081
     to_port     = 8081
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.vpc_cidr]
   }
 
   # SSH ingress: allow remote administration
@@ -49,7 +49,13 @@ resource "aws_security_group" "frontend_sg" {
     protocol    = "tcp"
     cidr_blocks = [var.admin_cidr]
   }
-
+  
+  ingress {
+    from_port = 3128
+    to_port = 3128
+    protocol = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
   egress {
     from_port   = 0
     to_port     = 0
